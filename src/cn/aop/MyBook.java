c2e09e684aba764065a65326de2a34d8a0417ab3
Ss <544915551@qq.com>
2018-04-26 08:22:29 +0800

    4.20

--- src/cn/aop/MyBook.java
+++ src/cn/aop/MyBook.java
@@ -22,7 +22,6 @@ public class MyBook {
     public void around1(ProceedingJoinPoint proceedingJoinPoint)throws Throwable{
         //方法之前
         System.out.println("方法之前......");
-
         //执行被增强的方法
         proceedingJoinPoint.proceed();
 
